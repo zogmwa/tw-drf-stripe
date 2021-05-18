3f763ff0b9adc6b52729cd33cc29421c1327a66a
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-05-18 02:13:20 -0400

    Add ES Document/index for Asset model

--- api/documents.py
+++ api/documents.py
@@ -5,16 +5,8 @@ from django_elasticsearch_dsl import Document, fields, Index
 from django_elasticsearch_dsl.registries import registry
 from elasticsearch_dsl import analyzer
 
-from api.models import Tag
+from api.models import Tag, Asset
 
-# The name of your index
-tag = Index('tag')
-
-# See Elasticsearch Indices API reference for available settings
-tag.settings(
-    number_of_shards=1,
-    number_of_replicas=0
-)
 
 # html_strip = analyzer(
 #     'html_strip',
@@ -23,14 +15,19 @@ tag.settings(
 #     char_filter=["html_strip"]
 # )
 
-
 @registry.register_document
-@tag.document
 class TagDocument(Document):
     """
-    Tag Elasticsearch document
+    Tag Elasticsearch Document
     """
     slug = fields.SearchAsYouTypeField()
+
+    class Index:
+        # Name of the Elasticsearch index
+        name = 'tag'
+        # See Elasticsearch Indices API reference for available settings
+        settings = {'number_of_shards': 1,
+                    'number_of_replicas': 0}
 
     class Django:
         model = Tag
@@ -41,6 +38,38 @@ class TagDocument(Document):
         # Ignore auto updating of Elasticsearch when a model is saved
         # or deleted:
         # ignore_signals = True
+
+        # Don't perform an index refresh after every update (overrides global setting):
+        # auto_refresh = False
+
+        # Paginate the django queryset used to populate the index with the specified size
+        # (by default it uses the database driver's default setting)
+        # queryset_pagination = 5000
+
+
+@registry.register_document
+class AssetDocument(Document):
+    """
+    Web Asset Elasticsearch Document
+    """
+    class Index:
+        # Name of the Elasticsearch index
+        name = 'asset'
+        # See Elasticsearch Indices API reference for available settings
+        settings = {'number_of_shards': 1,
+                    'number_of_replicas': 0}
+
+    class Django:
+        model = Asset
+        # The fields of the model you want to be indexed in Elasticsearch,
+        # other than the ones already used in the Document class
+        fields = [
+            'name',
+            'description',
+        ]
+
+        # Ignore auto updating of Elasticsearch when a model is saved or deleted.
+        ignore_signals = False
 
         # Don't perform an index refresh after every update (overrides global setting):
         # auto_refresh = False
