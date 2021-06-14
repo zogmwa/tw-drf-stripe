f9081abb7c8cbc37c9a7288425d3064544961c98
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-06-14 13:43:23 -0500

    Improve read_from_excel command to populate short_description, read tags column which is a list of strings

--- api/management/commands/load_assets_from_excel.py
+++ api/management/commands/load_assets_from_excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 from django.core.management import BaseCommand
 from django.utils.text import slugify
 from furl import furl
@@ -6,21 +7,28 @@ from furl import furl
 from api.models import Tag, Asset
 
 
-def process(tag_name: str, excel_path: str) -> None:
+def process(excel_path: str) -> None:
     df = pd.read_excel(excel_path)
-    tag, is_created = Tag.objects.get_or_create(name=tag_name, slug=slugify(tag_name))
 
     for i, row in df.iterrows():
-        name = row['Name'].strip()
-        slug = slugify(name)
-        website = row['Website'].strip()
-        description = row['Description'].strip()
+        tag_names = ast.literal_eval(row['tags'])
+        tags = set()
+
+        for tag_name in tag_names:
+            tag, is_created = Tag.objects.get_or_create(name=tag_name, slug=slugify(tag_name))
+            tags.add(tag)
+
+        name = row[Asset.name.field.attname].strip()
+        slug = slugify(name)[:50]
+        website = row[Asset.website.field.attname].strip()
+        print(website)
+        short_description = row[Asset.short_description.field.attname].strip()
         asset, is_created = Asset.objects.get_or_create(slug=slug, name=name)
         asset.website = website
-        asset.description = description
+        asset.short_description = short_description
         furled_url = furl(website)
         asset.logo_url = 'https://logo.clearbit.com/{}'.format(furled_url.netloc)
-        asset.tags.set([tag])
+        asset.tags.set(list(tags))
         asset.save()
 
 
@@ -31,5 +39,4 @@ class Command(BaseCommand):
             default_excel_path)
         ) or default_excel_path
 
-        tag_name = input("Enter a tag name:\n")
-        process(tag_name, excel_filepath)
+        process(excel_filepath)
