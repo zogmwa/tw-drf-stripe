5a4bddb9647abcfca13d4a0082e7cf079cdefff2
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-06-16 15:52:21 -0500

    Add Django command/CLI to merge two tags and tests
    The command will update the LinkedTag M2M model and delete the bad tag (after it is replaced with the good tag).
    
    Fixes #36

--- api/management/commands/merge_tags.py
+++ api/management/commands/merge_tags.py
@@ -3,6 +3,17 @@ from django.core.management.base import BaseCommand
 from api.models import Tag, Asset, LinkedTag
 
 
+def _merge_tags(t1, t2):
+    """
+    Replace bad tag t1 with good tag t2.
+    """
+    bad_tag = Tag.objects.get(name=t1)
+    good_tag = Tag.objects.get(name=t2)
+    linked_tags_qs = LinkedTag.objects.filter(tag=bad_tag)
+    linked_tags_qs.update(tag=good_tag)
+    bad_tag.delete()
+
+
 class Command(BaseCommand):
     help = 'Helps Merge Replace 2 tags (Replaces bad tag t1 with good tag t2). Updates M2M relationships.'
 
@@ -11,8 +22,5 @@ class Command(BaseCommand):
         parser.add_argument('--t2', type=str, help="The good tag")
 
     def handle(self, *args, **kwargs):
-        bad_tag = Tag.objects.filter(name=kwargs['t1'])
-        linked_tags_qs = LinkedTag.objects.filter(tag=bad_tag)
-        good_tag = Tag.objects.get(name=kwargs['t2'])
-        linked_tags_qs.update(tag=good_tag)
-        bad_tag.delete()
+        t1, t2 = kwargs['t1'], kwargs['t2']
+        _merge_tags(t1, t2)
---
+++ tests/test_merge_tags.py
@@ -0,0 +1,20 @@
+from api.management.commands.merge_tags import _merge_tags
+from api.models import Tag, Asset
+
+
+def test__merge_tags():
+    tag1_obj = Tag.objects.create(name='t1', slug='t1')
+    tag2_obj = Tag.objects.create(name='t2', slug='t2')
+    asset = Asset.objects.create(
+        name='Test Asset',
+        website='example.com',
+        short_description='Test',
+        description='Test',
+    )
+    asset.tags.set([tag1_obj])
+    asset.save()
+    assert asset.tags.get() == tag1_obj
+
+    _merge_tags(tag1_obj.name, tag2_obj.name)
+
+    assert asset.tags.get() == tag2_obj
