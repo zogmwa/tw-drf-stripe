b4385b100ca5d319d1dd83868cf0a7bfdadd47ea
Yug Mittal <33040186+mittal1787@users.noreply.github.com>
2021-07-24 13:44:49 -0500

    A command that cleans every asset's description and replaces every ex… (#70)
    * A command that cleans every asset's description and replaces every excel character with a specific one
    
    * Fix formatting of py file + improve efficiency via Asset.objects.bulk_update
    
    * Batch update assets

---
+++ api/management/commands/clean_descriptions.py
@@ -0,0 +1,34 @@
+from django.core.management import BaseCommand
+
+from api.models import Asset
+
+
+def clean_asset_descriptions() -> None:
+    asset_list = Asset.objects.all()
+    batch = []
+
+    for asset in asset_list:
+        if "_x000D_" in asset.description:
+            asset.description = asset.description.replace("_x000D_", "\n")
+
+        if "x000D" in asset.description:
+            asset.description = asset.description.replace("x000D", "\n")
+
+        if "_x000D_" in asset.short_description:
+            asset.short_description = asset.short_description.replace("_x000D_", "\n")
+
+        if "x000D" in asset.short_description:
+            asset.short_description = asset.short_description.replace("x000D", "\n")
+
+        batch.append(asset)
+
+        if len(batch) == 500:
+            Asset.objects.bulk_update(batch, ['description', 'short_description'])
+            batch = []
+
+    Asset.objects.bulk_update(batch, ['description', 'short_description'])
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        clean_asset_descriptions()
