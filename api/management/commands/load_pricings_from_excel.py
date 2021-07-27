f8fc7d88e1dd7a112bb3cdbb1e0436d87de8d311
Yug Mittal <33040186+mittal1787@users.noreply.github.com>
2021-07-27 15:12:43 -0500

    Add pricings command (#73)
    * A command that cleans every asset's description and replaces every excel character with a specific one
    
    * Batch update assets
    
    * Account for NoneType when cleaning descriptions
    
    * Add Voice over IP (VoIP) special tag + account for x000D and _x000D_ in description and short description when importing
    
    * Create command to load asset pricings from excel file

---
+++ api/management/commands/load_pricings_from_excel.py
@@ -0,0 +1,51 @@
+import json
+import logging
+
+import pandas as pd
+from django.core.management import BaseCommand
+from django.db import IntegrityError
+
+from api.models import Asset, PricePlan
+
+
+def process(excel_path: str) -> None:
+    df = pd.read_excel(excel_path)
+
+    for i, row in df.iterrows():
+        asset_slug = row[Asset.slug_name.field.attname].strip()
+        asset = Asset.objects.get_or_create(slug=asset_slug)
+
+        asset_pricings = json.loads(row['pricing_data'])  # The pricing data is in the form of a dictionary
+
+        if asset_slug in asset_pricings.keys():
+            asset_plans = asset_pricings[asset_slug]
+            for plan_name in asset_plans.keys():
+                price_plan, is_created = PricePlan.objects.get_or_create(asset=asset, name=plan_name)
+                plan_price_field_name = PricePlan.plan_price.field.attname
+                plan_per_field_name = PricePlan.plan_per.field.attname
+                plan_features_field_name = PricePlan.features.field.attname
+                plan_currency_field_name = PricePlan.currency.field.attname
+                plan_summary_field_name = PricePlan.summary.field.attname
+                price_plan.plan_price = asset_plans[plan_name][plan_price_field_name].strip()
+                price_plan.plan_per = asset_plans[plan_name][plan_per_field_name].strip()
+
+                if plan_summary_field_name in asset_plans[plan_name].keys():
+                    price_plan.features = asset_plans[plan_name][plan_summary_field_name].strip()
+
+                if plan_features_field_name in asset_plans[plan_name].keys():
+                    price_plan.features = asset_plans[plan_name][plan_features_field_name].strip()
+
+                if plan_currency_field_name in asset_plans[plan_name].keys():
+                    price_plan.currency = asset_plans[plan_name][plan_currency_field_name].strip()
+
+                price_plan.save()
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        default_excel_path = "../data/pricings.xlsx"
+        excel_filepath = input("Enter XLS/XLSX relative path for pricings or leave blank for default ({}):\n".format(
+            default_excel_path)
+        ) or default_excel_path
+
+        process(excel_filepath)
