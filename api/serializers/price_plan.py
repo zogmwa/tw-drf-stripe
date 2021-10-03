e0ed36c63cdb45e8e26a53188d694f3b9992d904
zogmwa <zogmwatech@gmail.com>
2021-10-03 15:06:06 -0700

    Code formatting

--- api/serializers/price_plan.py
+++ api/serializers/price_plan.py
@@ -6,4 +6,13 @@ from api.models import PricePlan
 class PricePlanSerializer(ModelSerializer):
     class Meta:
         model = PricePlan
-        fields = ['asset', 'name', 'summary', 'currency', 'price', 'per', 'features', 'most_popular']
+        fields = [
+            'asset',
+            'name',
+            'summary',
+            'currency',
+            'price',
+            'per',
+            'features',
+            'most_popular',
+        ]
