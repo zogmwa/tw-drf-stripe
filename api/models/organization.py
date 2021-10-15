52cb7f2a517c29e845c0d189d42d30eb743d1b4f
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-10-15 17:25:21 -0400

    Add organization logo as this can be rendered on the Service details page on the frontend (for Companies Using section)

--- api/models/organization.py
+++ api/models/organization.py
@@ -9,6 +9,7 @@ class Organization(models.Model):
 
     name = models.CharField(max_length=255, unique=True)
     website = models.URLField(max_length=2048, null=True, blank=True)
+    logo_url = models.URLField(max_length=2048, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- api/serializers/organization.py
+++ api/serializers/organization.py
@@ -6,7 +6,8 @@ from api.models import Organization
 class OrganizationSerializer(ModelSerializer):
     class Meta:
         model = Organization
-        fields = ['name']
+        fields = ['name', 'website', 'logo_url']
         extra_kwargs = {
             'name': {'validators': []},
         }
+        read_only_fields = ['website', 'logo_url']
