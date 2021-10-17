9e3f20d92b9c58c8b4b8c3e399afbfe2f3312264
zogmwa <zogmwatech@gmail.com>
2021-10-18 05:56:43 +0600

    Returned 'review count', 'avg_rating' of asset when posting review

--- api/serializers/asset_review.py
+++ api/serializers/asset_review.py
@@ -1,13 +1,15 @@
-from django.db.models.signals import pre_save, post_save
-from django.dispatch import receiver
 from rest_framework.serializers import ModelSerializer
-
-from api.models import AssetReview
+from rest_framework import serializers
+from api.models import AssetReview, Asset
 from api.serializers.user import UserSerializer
 
 
 class AssetReviewSerializer(ModelSerializer):
     user = UserSerializer(read_only=True)
+    asset_reviews_count = serializers.IntegerField(read_only=True)
+    asset_avg_rating = serializers.DecimalField(
+        read_only=True, decimal_places=7, max_digits=10
+    )
 
     class Meta:
         model = AssetReview
@@ -19,6 +21,8 @@ class AssetReviewSerializer(ModelSerializer):
             'rating',
             'created',
             'video_url',
+            'asset_reviews_count',
+            'asset_avg_rating',
         ]
 
     def _inject_logged_in_user_into_validated_data(self, validated_data: dict):
@@ -33,7 +37,11 @@ class AssetReviewSerializer(ModelSerializer):
         return validated_data
 
     def create(self, validated_data):
-        return super().create(validated_data)
+        instance = super().create(validated_data)
+        asset = Asset.objects.get(pk=instance.asset.id)
+        instance.asset_reviews_count = asset.reviews_count
+        instance.asset_avg_rating = asset.avg_rating
+        return instance
 
     def update(self, instance, validated_data):
         logged_in_user = self.context['request'].user
