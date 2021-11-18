ff11210d51db67c5e584dfa2e59d6b83b58fd70c
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-11-18 16:24:10 -0600

    Fix stripe webhook path (djstripe already has /stripe prefix for the webhook)
    The webhook after this PR should reside at /stripe/webhook/

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from djstripe import webhooks
+
+
+@webhooks.handler("checkout.session.completed")
+def checkout_session_completed_handler(event, **kwargs):
+    # TODO: Create a SolutionBooking object with payment pending
+    print("We should probably notify the user at this point by redirecting to frontend")
