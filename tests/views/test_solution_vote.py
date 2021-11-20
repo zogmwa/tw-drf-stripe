eeaefd323f1b3f3bf3fc4c11a48ec318fa2356a6
zogmwa <zogmwatech@gmail.com>
2021-11-19 16:30:46 -0800

    Added unauthenticated user vote solution test case.

--- tests/views/test_solution_vote.py
+++ tests/views/test_solution_vote.py
@@ -9,11 +9,21 @@ SOLUTION_VOTES_ENDPOINT = 'http://127.0.0.1:8000/solution_votes/'
 class TestSolutionVote:
     def test_create_with_logged_in_user(
         self,
+        unauthenticated_client,
         authenticated_client,
         user_and_password,
         example_solution,
     ):
-        # Vote on solution
+        # Vote on solution with unauthenticated user
+        response = unauthenticated_client.post(
+            SOLUTION_VOTES_ENDPOINT,
+            {
+                'solution': example_solution.id,
+            },
+        )
+        assert response.status_code == 401
+
+        # Vote on solution with authenticated user
         response = authenticated_client.post(
             SOLUTION_VOTES_ENDPOINT,
             {
@@ -26,14 +36,26 @@ class TestSolutionVote:
 
     def test_get_and_delete(
         self,
+        unauthenticated_client,
         authenticated_client,
         user_and_password,
         example_solution,
     ):
+        # Cancel vote on solution with unauthenticated user
         solution_vote_to_be_deleted = SolutionVote.objects.create(
             solution=example_solution, user=user_and_password[0]
         )
 
+        solution_vote_detail_endpoint = "{}{}/".format(
+            SOLUTION_VOTES_ENDPOINT, solution_vote_to_be_deleted.id
+        )
+        get_solution_votes_response = unauthenticated_client.get(
+            solution_vote_detail_endpoint
+        )
+
+        assert get_solution_votes_response.status_code == 401
+
+        # Cancel vote on solution with authenticated user
         solution_vote_detail_endpoint = "{}{}/".format(
             SOLUTION_VOTES_ENDPOINT, solution_vote_to_be_deleted.id
         )
