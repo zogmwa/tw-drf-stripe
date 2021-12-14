5b90ed8a00b63d95b34b9cbb705e6b27665a2cfd
zogmwa <zogmwatech@gmail.com>
2021-12-13 17:45:51 -0800

    Added django-apscheduler settings

--- taggedweb/settings.py
+++ taggedweb/settings.py
@@ -81,6 +81,8 @@ INSTALLED_APPS = [
     # Project Apps
     'api',
     'payments',
+    # Scheduled Job
+    'django_apscheduler',
 ]
 
 SITE_ID = 1
@@ -304,6 +306,24 @@ STRIPE_LIVE_MODE = False
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# django-apscheduler setting
+
+# Format string for displaying run time timestamps in the Django admin site. The default
+# just adds seconds to the standard Django format, which is useful for displaying the timestamps
+# for jobs that are scheduled to run on intervals of less than one minute.
+#
+# See https://docs.djangoproject.com/en/dev/ref/settings/#datetime-format for format string
+# syntax details.
+APSCHEDULER_DATETIME_FORMAT = "%Y-%m-%d %H:%M"
+
+# Maximum run time allowed for jobs that are triggered manually via the Django admin site, which
+# prevents admin site HTTP requests from timing out.
+#
+# Longer running jobs should probably be handed over to a background task processing library
+# that supports multiple background worker processes instead (e.g. Dramatiq, Celery, Django-RQ,
+# etc. See: https://djangopackages.org/grids/g/workers-queues-tasks/ for popular options).
+APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
 
 try:
     # Over-ride settigs be defined for customization on production servers/local
