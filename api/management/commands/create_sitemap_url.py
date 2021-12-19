046a09f0af1be050bdf88debe8f8c28c19cb6494
zogmwa <zogmwatech@gmail.com>
2021-12-19 08:34:18 -0800

    Updated split sitemap create and compress them.

--- api/management/commands/create_sitemap_url.py
+++ api/management/commands/create_sitemap_url.py
@@ -3,70 +3,177 @@ Allows Making sitemap.xml file from assets and solutions.
 """
 from django.core.management.base import BaseCommand
 
-from io import BytesIO
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
+import codecs
 import datetime
-from itertools import combinations
 from django.core.files.storage import default_storage
+from gzip import GzipFile
+import gzip
+from django.conf import settings
 
 from api.models.solution import Solution
 from api.models.asset import Asset
+from api.models.tag import Tag
 
 
 def process() -> None:
     """
     Output sitemap urls to given sitemap.xml location.
     """
-    generated_url = set()
-    data = ET.Element('urlset')
-    data.set('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
-    data.set('xmlns:news', 'http://www.google.com/schemas/sitemap-news/0.9')
-    data.set('xmlns:xhtml', 'http://www.w3.org/1999/xhtml')
-    data.set('xmlns:mobile', 'http://www.google.com/schemas/sitemap-mobile/1.0')
-    data.set('xmlns:image', 'http://www.google.com/schemas/sitemap-image/1.1')
-    data.set('xmlns:video', 'http://www.google.com/schemas/sitemap-video/1.1')
+    print(datetime.datetime.now())
+    max_url_per_sitemap = (
+        50000  # maximum url of each sitemap - google recommended it should be 50K.
+    )
+    sitemap_file_index = 1  # each sitemap file index.
+    current_url_count = 0  # url counter of each sitemap file
+    sitemap_index_url = set()  # sitemap files' url saver.
+    sitemap_config_str = """<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">"""
+    sitemap_end_str = """
+</urlset>
+"""
+    current_output_filename = './static/sitemap_{}.xml.gz'.format(
+        sitemap_file_index
+    )  # First file created.
+    sitemap_index_url.add('sitemap_{}.xml.gz'.format(sitemap_file_index))
+    current_gzip_file = GzipFile(current_output_filename, 'w')
+    # Write sitemap file content.
+    current_gzip_file.write(sitemap_config_str.encode())
+    # Write homepage and software list page url to sitemap.
+    write_xml_str = """
+<url><loc>https://www.taggedweb.com/</loc><changefreq>weekly</changefreq><priority>0.7</priority><lastmod>{}</lastmod></url>
+<url><loc>https://www.taggedweb.com/softwares</loc><changefreq>weekly</changefreq><priority>0.7</priority><lastmod>{}</lastmod></url>
+<url><loc>https://www.taggedweb.com/solutions</loc><changefreq>weekly</changefreq><priority>0.7</priority><lastmod>{}</lastmod></url>""".format(
+        datetime.datetime.now(), datetime.datetime.now(), datetime.datetime.now()
+    )
+    current_gzip_file.write(write_xml_str.encode())
+    current_url_count = current_url_count + 2
 
-    generated_url.add('https://www.taggedweb.com/')
-    generated_url.add('https://www.taggedweb.com/softwares/')
-
-    # conbinations doc: https://www.geeksforgeeks.org/itertools-combinations-module-python-print-possible-combinations/
-    pairs_iterator = combinations(Asset.objects.values('slug'), 2)
-    for compare_slug in pairs_iterator:
-        generated_url.add(
-            'https://www.taggedweb.com/compare/'
-            + compare_slug[0]['slug']
-            + '-vs-'
-            + compare_slug[1]['slug']
-            + '/'
+    # Write software list pages' url to sitemap.
+    for chunk_tags in Tag.objects.values('slug').iterator(chunk_size=100):
+        write_xml_str = """
+<url><loc>https://www.taggedweb.com/softwares/{}</loc><changefreq>weekly</changefreq><priority>0.7</priority><lastmod>{}</lastmod></url>""".format(
+            chunk_tags['slug'], datetime.datetime.now()
         )
+        current_gzip_file.write(write_xml_str.encode())
+        current_url_count = current_url_count + 1
 
-    for chunk_solutions in Solution.objects.values('slug').iterator(chunk_size=1000):
-        generated_url.add(
-            'https://www.taggedweb.com/solution/' + chunk_solutions['slug'] + '/'
+        # if current urls count is 50K we should create a new sitemap file.
+        if current_url_count == max_url_per_sitemap:
+            current_gzip_file.write(sitemap_end_str.encode())
+            current_gzip_file.close()
+            current_url_count = 0
+            sitemap_file_index = sitemap_file_index + 1
+            current_output_filename = './static/sitemap_{}.xml.gz'.format(
+                sitemap_file_index
+            )
+            current_gzip_file = GzipFile(current_output_filename, 'w')
+            sitemap_index_url.add('sitemap_{}.xml.gz'.format(sitemap_file_index))
+            current_gzip_file.write(sitemap_config_str.encode())
+
+    for chunk_solutions in Solution.objects.values('slug').iterator(chunk_size=100):
+        write_xml_str = """
+<url><loc>https://www.taggedweb.com/solution/{}</loc><changefreq>weekly</changefreq><priority>0.7</priority><lastmod>{}</lastmod></url>""".format(
+            chunk_solutions['slug'], datetime.datetime.now()
         )
+        current_gzip_file.write(write_xml_str.encode())
+        current_url_count = current_url_count + 1
 
-    for url in generated_url:
-        add_xml_data = ET.SubElement(data, 'url')
-        sub_loc = ET.SubElement(add_xml_data, 'loc')
-        sub_loc.text = url
-        sub_changefreq = ET.SubElement(add_xml_data, 'changefreq')
-        sub_changefreq.text = 'daily'
-        sub_priority = ET.SubElement(add_xml_data, 'priority')
-        sub_priority.text = '0.7'
-        sub_lastmode = ET.SubElement(add_xml_data, 'lastmod')
-        sub_lastmode.text = str(datetime.datetime.now())
+        # if current urls count is 50K we should create a new sitemap file.
+        if current_url_count == max_url_per_sitemap:
+            current_gzip_file.write(sitemap_end_str.encode())
+            current_gzip_file.close()
+            current_url_count = 0
+            sitemap_file_index = sitemap_file_index + 1
+            current_output_filename = './static/sitemap_{}.xml.gz'.format(
+                sitemap_file_index
+            )
+            current_gzip_file = GzipFile(current_output_filename, 'w')
+            sitemap_index_url.add('sitemap_{}.xml.gz'.format(sitemap_file_index))
+            current_gzip_file.write(sitemap_config_str.encode())
 
-    et = ET.ElementTree(data)
-    f = BytesIO()
-    et.write(f, encoding='utf-8')
-    xml = f.getvalue()
-    xmlstr = minidom.parseString(xml).toprettyxml(indent="   ")
-    print(xmlstr)
+    current_gzip_file.write(sitemap_end_str.encode())
+    current_gzip_file.close()
+    current_url_count = 0
+    sitemap_file_index = sitemap_file_index + 1
+    current_output_filename = './static/sitemap_{}.xml.gz'.format(sitemap_file_index)
+    current_gzip_file = GzipFile(current_output_filename, 'w')
+    sitemap_index_url.add('sitemap_{}.xml.gz'.format(sitemap_file_index))
+    current_gzip_file.write(sitemap_config_str.encode())
 
+    # Create software compare pages' url in sitemap
+    for chunk_softwares1 in Asset.objects.all()[:20].iterator(chunk_size=100):
+        chunk_softwares1_tags = set(
+            chunk_softwares1.tags.values_list('slug', flat=True)
+        )
+        for chunk_softwares2 in Asset.objects.all()[:20].iterator(chunk_size=100):
+            if chunk_softwares1.slug >= chunk_softwares2.slug:
+                continue
+            chunk_softwares2_tags = set(
+                chunk_softwares2.tags.values_list('slug', flat=True)
+            )
+
+            # check if two list has common element
+            if chunk_softwares1_tags & chunk_softwares2_tags:
+                current_url_count = current_url_count + 1
+                write_xml_str = """
+<url><loc>https://www.taggedweb.com/compare/{}-vs-{}</loc><changefreq>weekly</changefreq><priority>0.7</priority><lastmod>{}</lastmod></url>""".format(
+                    chunk_softwares1.slug,
+                    chunk_softwares2.slug,
+                    datetime.datetime.now(),
+                )
+                current_gzip_file.write(write_xml_str.encode())
+                # if current urls count is 50K we should create a new sitemap file.
+                if current_url_count == max_url_per_sitemap:
+                    current_gzip_file.write(sitemap_end_str.encode())
+                    current_gzip_file.close()
+                    current_url_count = 0
+                    sitemap_file_index = sitemap_file_index + 1
+                    current_output_filename = './static/sitemap_{}.xml.gz'.format(
+                        sitemap_file_index
+                    )
+                    current_gzip_file = GzipFile(current_output_filename, 'w')
+                    sitemap_index_url.add(
+                        'sitemap_{}.xml.gz'.format(sitemap_file_index)
+                    )
+                    current_gzip_file.write(sitemap_config_str.encode())
+
+    current_gzip_file.write(sitemap_end_str.encode())
+    current_gzip_file.close()
+
+    # Create sitemap index file content.
+    sitemap_index_content = """<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
+    # Upload sitemap files to S3.
+    for url in sitemap_index_url:
+        sitemap_index_content += """
+<sitemap>
+    <loc>{}{}</loc>
+    <lastmod>{}</lastmod>
+</sitemap>""".format(
+            settings.STATIC_URL, url, datetime.datetime.now()
+        )
+        file = default_storage.open(url, 'w')
+        split_file = gzip.open('./static/{}'.format(url), 'rb')
+        split_content = split_file.read()
+        file.write(gzip.compress(split_content))
+        file.close()
+
+    current_gzip_file = codecs.open('./static/sitemap.xml', 'w', 'utf-8')
+
+    sitemap_index_content += """
+</sitemapindex>
+"""
+    current_gzip_file.write(sitemap_index_content)
+    # Upload sitemap index file to S3.
     file = default_storage.open('sitemap.xml', 'w')
-    file.write(xmlstr.encode('utf-8'))
+    file.write(sitemap_index_content.encode('utf-8'))
     file.close()
+
+    print(datetime.datetime.now())
 
 
 class Command(BaseCommand):
