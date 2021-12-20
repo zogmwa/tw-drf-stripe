ddb6b05164f386c377f8622861c2554f802bbdc6
zogmwa <zogmwatech@gmail.com>
2021-12-20 12:06:12 -0800

    Added authenticated user serializer

--- api/serializers/solution_booking.py
+++ api/serializers/solution_booking.py
@@ -1,9 +1,35 @@
 from rest_framework.serializers import ModelSerializer
 
 from api.models.solution_booking import SolutionBooking
+from api.serializers.solution import SolutionSerializer
 
 
 class SolutionBookingSerializer(ModelSerializer):
+    class Meta:
+        model = SolutionBooking
+        fields = [
+            'solution',
+            'booked_by',
+            'manager',
+            'status',
+            'created',
+            'updated',
+            'provider_notes',
+        ]
+        read_only_fields = [
+            'solution',
+            'booked_by',
+            'manager',
+            'status',
+            'created',
+            'updated',
+            'provider_notes',
+        ]
+
+
+class AuthenticatedSolutionBookingSerializer(ModelSerializer):
+    solution = SolutionSerializer(read_only=True)
+
     class Meta:
         model = SolutionBooking
         fields = [
