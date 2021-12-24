8b182c29d1febc931d791c3cc62a5c5dfd3799ce
zogmwa <zogmwatech@gmail.com>
2021-12-24 13:24:33 -0800

    Added try catch block to creating solution reviews block

--- api/views/solution_reviews.py
+++ api/views/solution_reviews.py
@@ -12,49 +12,40 @@ class SolutionReviewViewSet(viewsets.ModelViewSet):
     serializer_class = SolutionReviewSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = {
-        'solution_id': ['exact'],
+        'solution__id': ['exact'],
         'user__username': ['exact'],
     }
-
-    def get_queryset(self):
-        if self.action == 'list':
-            reviews = SolutionReview.objects.all()
-            return reviews
-        elif self.action == 'retrieve' or self.action == 'destroy':
-            solution_reivew_id = self.kwargs['pk']
-            return SolutionReview.objects.filter(
-                id=solution_reivew_id, user=self.request.user
-            )
-        else:
-            super(SolutionReviewViewSet, self).get_queryset()
 
     def create(self, request, *args, **kwargs):
         user = self.request.user
         solution_id = self.request.data.get('solution')
         review_type = self.request.data.get('type')
-        solution = Solution.objects.get(id=solution_id)
-
         try:
-            solution_review = SolutionReview.objects.get(
-                user=user,
-                solution=solution,
-                type=review_type,
-            )
-        except SolutionReview.DoesNotExist:
+            solution = Solution.objects.get(id=solution_id)
+
             try:
                 solution_review = SolutionReview.objects.get(
                     user=user,
                     solution=solution,
-                )
-                solution_review.type = review_type
-                solution_review.save()
-            except SolutionReview.DoesNotExist:
-                solution_review = SolutionReview.objects.create(
-                    user=user,
-                    solution=solution,
                     type=review_type,
                 )
+            except SolutionReview.DoesNotExist:
+                try:
+                    solution_review = SolutionReview.objects.get(
+                        user=user,
+                        solution=solution,
+                    )
+                    solution_review.type = review_type
+                    solution_review.save()
+                except SolutionReview.DoesNotExist:
+                    solution_review = SolutionReview.objects.create(
+                        user=user,
+                        solution=solution,
+                        type=review_type,
+                    )
 
-        solution_review_serializer = SolutionReviewSerializer(solution_review)
+            solution_review_serializer = SolutionReviewSerializer(solution_review)
 
-        return Response(solution_review_serializer.data)
+            return Response(solution_review_serializer.data)
+        except Solution.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
