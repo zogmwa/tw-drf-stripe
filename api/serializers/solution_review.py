5b4b6f0ceb7f666a7988e3a03ba7b5142a8272d1
zogmwa <zogmwatech@gmail.com>
2021-12-25 18:33:41 -0800

    Added avg_rating field to solution review serializer

--- api/serializers/solution_review.py
+++ api/serializers/solution_review.py
@@ -3,14 +3,30 @@ from api.models import SolutionReview, Solution
 
 
 class SolutionSerializerForSolutionReview(serializers.ModelSerializer):
-    model = Solution
+    avg_rating = serializers.SerializerMethodField(
+        method_name="_get_solution_review_avg_rating"
+    )
 
     class Meta:
         model = Solution
         fields = [
             'id',
             'slug',
+            'avg_rating',
         ]
+
+    def _get_solution_review_avg_rating(self, solution_instance):
+        """
+        Calculating avg_rating from status counts
+        """
+        sad_count = solution_instance.sad_count
+        happy_count = solution_instance.happy_count
+        if happy_count - sad_count > 0:
+            return 1
+        elif happy_count - sad_count == 0:
+            return 0
+        else:
+            return -1
 
 
 class SolutionReviewSerializer(serializers.ModelSerializer):
