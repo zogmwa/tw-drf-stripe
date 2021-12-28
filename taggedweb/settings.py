a0fd5bbb25c445e134f2649e10c5507e8a15a933
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-12-28 02:34:37 -0500

    Add sentry setup

--- taggedweb/settings.py
+++ taggedweb/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -320,6 +322,25 @@ APSCHEDULER_DATETIME_FORMAT = "%Y-%m-%d %H:%M"
 # that supports multiple background worker processes instead (e.g. Dramatiq, Celery, Django-RQ,
 # etc. See: https://djangopackages.org/grids/g/workers-queues-tasks/ for popular options).
 APSCHEDULER_RUN_NOW_TIMEOUT = 7200  # Seconds
+
+
+sentry_sdk.init(
+    dsn="https://ae3e3499901e40a0a7848f5485d32587@o1064580.ingest.sentry.io/6055486",
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
+
 
 try:
     # Over-ride settigs be defined for customization on production servers/local
