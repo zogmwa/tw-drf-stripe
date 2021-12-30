a2762c0ad929ba78163fa1777fe54c7308628989
zogmwa <zogmwatech@gmail.com>
2021-12-30 06:27:58 -0800

    Added cancelled status to solution booking

--- api/models/solution_booking.py
+++ api/models/solution_booking.py
@@ -17,6 +17,7 @@ class SolutionBooking(models.Model):
     """
 
     class Status(models.TextChoices):
+        CANCELLED = 'Cancelled'
         PENDING = 'Pending'
         IN_PROGRESS = 'In Progress'
         IN_REVIEW = 'In Review'
@@ -92,21 +93,15 @@ def count_update_for_new_or_complete_booking(sender, instance=None, **kwargs):
                     )
                     - 1,
                 )
-        else:
-            if instance.status != sender.Status.COMPLETED:
-                Solution.objects.filter(id=instance.solution.id).update(
-                    bookings_pending_fulfillment_count=F(
-                        'bookings_pending_fulfillment_count'
-                    )
-                    + 1,
-                )
-
     except sender.DoesNotExist:
         # New solution booking is being added
-        Solution.objects.filter(id=instance.solution.id).update(
-            bookings_pending_fulfillment_count=F('bookings_pending_fulfillment_count')
-            + 1,
-        )
+        if instance.status != SolutionBooking.Status.CANCELLED:
+            Solution.objects.filter(id=instance.solution.id).update(
+                bookings_pending_fulfillment_count=F(
+                    'bookings_pending_fulfillment_count'
+                )
+                + 1,
+            )
 
 
 @receiver(pre_save, sender=SolutionBooking)
@@ -124,11 +119,15 @@ def set_created_at_field_when_solution_status_from_pending_to_others(
     try:
         old_contract_instance = sender.objects.get(pk=contract_instance.pk)
         if old_contract_instance.status == sender.Status.PENDING:
-            if instance.status != sender.Status.PENDING:
+            if (instance.status != sender.Status.PENDING) and (
+                instance.status != sender.Status.CANCELLED
+            ):
                 instance.started_at = get_now_converted_google_date()
 
     except sender.DoesNotExist:
-        if instance.status != sender.Status.PENDING:
+        if (instance.status != sender.Status.PENDING) and (
+            instance.status != sender.Status.CANCELLED
+        ):
             instance.started_at = datetime.datetime.now().date()
 
 
