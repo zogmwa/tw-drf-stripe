bfa678cc2a7601389e8f9cd0f5ae3e31a38cd711
zogmwa <zogmwatech@gmail.com>
2022-01-31 04:36:20 +0000

    Added time tracked day model

---
+++ api/models/time_tracked_day.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models import UniqueConstraint
+from api.models.user import User
+from api.models.solution_booking import SolutionBooking
+
+
+class TimeTrackedDay(models.Model):
+    """ """
+
+    solution_booking = models.ForeignKey(
+        SolutionBooking, null=True, on_delete=models.CASCADE
+    )
+
+    # Who is submitting these hours (It could be on behalf of the provider)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+    date = models.DateTimeField(null=True)
+    tracked_hours = models.DecimalField(null=True, max_digits=2, decimal_places=2)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['solution_booking', 'date'], name='booking_tracked_date'
+            )
+        ]
+
+        verbose_name = 'Time Tracked Day'
+        verbose_name_plural = 'Time Tracked Days'
