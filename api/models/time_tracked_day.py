3ddc9fae6440652f49716f9797f69d4fa7d4b1ea
zogmwa <zogmwatech@gmail.com>
2022-02-02 13:55:08 -0500

    Fixed time tracked day model

--- api/models/time_tracked_day.py
+++ api/models/time_tracked_day.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import UniqueConstraint
 from api.models.user import User
 from api.models.solution_booking import SolutionBooking
+import django.utils.timezone
 
 
 class TimeTrackedDay(models.Model):
@@ -9,15 +10,13 @@ class TimeTrackedDay(models.Model):
     This model is for storing the tracked times instances.
     """
 
-    solution_booking = models.ForeignKey(
-        SolutionBooking, null=True, on_delete=models.CASCADE
-    )
+    solution_booking = models.ForeignKey(SolutionBooking, on_delete=models.CASCADE)
 
     # Who is submitting these hours (It could be on behalf of the provider)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 
-    date = models.DateTimeField(null=True)
-    tracked_hours = models.DecimalField(null=True, max_digits=2, decimal_places=1)
+    date = models.DateTimeField(default=django.utils.timezone.now)
+    tracked_hours = models.DecimalField(default=0, max_digits=2, decimal_places=1)
 
     class Meta:
         constraints = [
