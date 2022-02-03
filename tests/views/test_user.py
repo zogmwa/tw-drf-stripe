d86e5fc3e79f27827db6968a62af5010df2bb732
zogmwa <zogmwatech@gmail.com>
2022-02-03 00:27:43 -0500

    Added test code

--- tests/views/test_user.py
+++ tests/views/test_user.py
@@ -13,6 +13,10 @@ from djstripe.models import Price as StripePrice
 from djstripe.models import Product as StripeProduct
 from tests.common import get_random_string
 from tests.views.test_asset import _create_asset
+from api.models.webhooks_stripe import (
+    subscription_updated_handler,
+)
+from djstripe.models import Subscription, Event
 
 USERS_BASE_ENDPOINT = 'http://127.0.0.1:8000/users/'
 
@@ -729,3 +733,46 @@ class TestProviderBookingsList:
         assert response.status_code == 200
         assert response.data['tracking_times'][0]['tracked_hours'] == '2.0'
         assert response.data['tracking_times'][1]['tracked_hours'] == '1.0'
+
+
+class TestStripeSubscriptionHandler:
+    @override_settings(STRIPE_TEST_PUBLISHED_KEY='')
+    def test_subscription_updated_webhook_should_be_sync_subscription_model(
+        self,
+        example_solution,
+        example_stripe_product_create_event,
+        example_stripe_price_create_event,
+        example_stripe_subscription_object,
+        example_stripe_customer_object,
+        example_stripe_attach_payment_method_customer_object_1,
+        example_stripe_attach_payment_method_customer_object_2,
+        example_stripe_customer_has_default_payment_method_object,
+        user_and_password,
+        authenticated_client,
+        mocker,
+    ):
+        # Create a new subscribe
+        test_user_payment = TestUserPayment()
+        test_user_payment._customer_subscribe_payment(
+            mocker,
+            example_stripe_subscription_object,
+            example_stripe_price_create_event,
+            example_stripe_product_create_event,
+            example_solution,
+            user_and_password,
+            example_stripe_attach_payment_method_customer_object_1,
+            authenticated_client,
+            example_stripe_customer_object,
+        )
+
+        # Update subscribe using webhook
+        example_stripe_subscription_event = Event(
+            data={
+                'object': util.convert_to_stripe_object(
+                    example_stripe_subscription_object
+                )
+            }
+        )
+        subscription_updated_handler(example_stripe_subscription_event)
+
+        assert Subscription.objects.count() == 1
