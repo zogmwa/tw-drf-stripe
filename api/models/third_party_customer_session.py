8c9d2f2f8533a862e7663d9e6751db88481ccf0b
zogmwa <zogmwatech@gmail.com>
2022-02-15 14:18:28 +0100

    Added third_party_customer_session model

---
+++ api/models/third_party_customer_session.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.db.models import UniqueConstraint
+from api.models.third_party_customer import ThirdPartyCustomer
+
+
+class ThirdPartyCustomerSession(models.Model):
+    """
+    Third party customer session is for store the third party customer's session info.
+    """
+
+    third_party_customer = models.OneToOneField(
+        ThirdPartyCustomer,
+        related_name='third_party_customer',
+        on_delete=models.CASCADE,
+    )
+
+    session_id = models.CharField(max_length=255)
+
+    expire_date = models.DateTimeField(null=True, blank=True)
+
+    is_expired = models.BooleanField(
+        default=False,
+        help_text='When payment action is finished then this value should be set to True.',
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['session_id'], name='third_party_customer_session_id'
+            )
+        ]
+        verbose_name = 'Third Party Customer Session'
+        verbose_name_plural = 'Third Party Customer Sessions'
